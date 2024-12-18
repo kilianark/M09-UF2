ce892e74a38b848ac09886f87fdbbda14dece0ab
Kilian Morillas <kilian08momo@gmail.com>
2024-12-18 19:47:36 +0100

    Renamed 02-Fils to 02-Futbol + created MainDemoFil.java wich demostrates java executes in threads

---
+++ 02-Futbol/res/MainDemoFil.java
@@ -0,0 +1,12 @@
+package res;
+
+public class MainDemoFil {
+    public static void main(String[] args) {
+        Thread filActual = Thread.currentThread();
+
+        System.out.println("MainDemoFil.main:");
+        System.out.println("Prioritat -> " + filActual.getPriority());
+        System.out.println("Nom -> " + filActual.getName());
+        System.out.println("toString() -> " + filActual.toString());
+    }
+}
