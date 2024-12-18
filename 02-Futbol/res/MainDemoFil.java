707d784eefd8d1f99f85bafad5373a0b0934ed3c
Kilian Morillas <kilian08momo@gmail.com>
2024-12-18 20:19:35 +0100

    Activitat 02-Futbol: commit final de classe

--- 02-Futbol/res/MainDemoFil.java
+++ 02-Futbol/res/MainDemoFil.java
@@ -5,7 +5,7 @@ public class MainDemoFil {
         Thread filActual = Thread.currentThread();
 
         System.out.println("MainDemoFil.main:");
-        System.out.println("Prioritat -> " + filActual.getPriority());
+        System.out.print("Prioritat -> " + filActual.getPriority() + ", ");
         System.out.println("Nom -> " + filActual.getName());
         System.out.println("toString() -> " + filActual.toString());
     }
